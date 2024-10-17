58da594076cd3ae4ac74cc430a79a35fb819c1dd
Baraa Nayyal <baraanayal0@gmail.com>
2024-10-17 21:30:01 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
+    
     MongooseModule.forRoot(process.env.MONGO_URL),
     DivisionsModule,
     ClassesModule,
