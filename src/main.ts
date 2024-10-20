4993f1d0067a23327a386228e26e0906adb03156
Baraa Nayyal <baraanayal0@gmail.com>
2024-10-20 15:52:49 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,9 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma, tz-offset',
   });
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
