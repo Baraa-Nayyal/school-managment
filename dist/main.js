9bbc5ac3bc42c1c3d2ca33320e0d33e88583ac4b
Baraa Nayyal <baraanayal0@gmail.com>
2024-10-20 15:35:46 +0300

    .

--- dist/main.js
+++ dist/main.js
@@ -12,7 +12,7 @@ async function bootstrap() {
         origin: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true,
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+        allowedHeaders: ['Content-Type', 'Authorization'],
     });
     await app.listen(process.env.PORT || 3000);
 }
